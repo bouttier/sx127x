979fe2472227303a477b56e05e7fb39729d2856b
Élie Bouttier <elie@bouttier.eu>
2025-02-19 18:22:48 +0100

    xxx

--- extra_script.py
+++ extra_script.py
@@ -1,6 +1,7 @@
-Import('env')
+Import("env")
 
-if env.get("PIOPLATFORM") == "espressif32":
+if env.get("PIOPLATFORM") == "XXXespressif32":
     env.Replace(SRC_FILTER=["+<*>", "-<*_spi*>", "+<*_esp_spi.c>"])
 else:
     env.Replace(SRC_FILTER=["+<*>", "-<*_spi*>"])
+
